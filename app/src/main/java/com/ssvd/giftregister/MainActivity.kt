c7952ce091cb23692eb60bbf0dca63810c28dd9a
sathish2422 <118890719+sathish2422@users.noreply.github.com>
2022-12-27 12:01:03 +0530

    Adding Button

--- app/src/main/java/com/ssvd/giftregister/MainActivity.kt
+++ app/src/main/java/com/ssvd/giftregister/MainActivity.kt
@@ -3,13 +3,15 @@ package com.ssvd.giftregister
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ssvd.giftregister.ui.theme.GiftRegisterTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,8 +34,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
 
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "Hello $name!")
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Submit")
+            
+        }
+    }
     
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
